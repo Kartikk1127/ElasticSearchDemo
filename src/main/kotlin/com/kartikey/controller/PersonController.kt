cdeb772714a7dd5972f7c14e1e1b3744e9b7aa1a
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2023-07-22 13:11:49 +0530

    api url change

--- src/main/kotlin/com/kartikey/controller/PersonController.kt
+++ src/main/kotlin/com/kartikey/controller/PersonController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/api")
 class PersonController(
     val personRepository: PersonRepository
 ) {
